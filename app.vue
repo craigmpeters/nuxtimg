067e28b3a65b1ea69c066ecd90972b7fc65e1e42
craigpeters@me.com <craigpeters@me.com>
2025-02-22 20:44:25 +0000

    initial test

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtImg fit="cover" :src="image" width="300px" height="300px" class="rounded object-contain w-96"  />
+  </div>
+</template>
+
+<script setup>
+
+const image = "/4c4ac901d16d7e0a96abb3ebcb0a83c9.jpg"
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
